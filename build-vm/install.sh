7cae7194d6462930202d2380f1215a8b724ff9e7
jodeck80 <jodeck80@gmail.com>
2015-11-28 22:53:26 -0500

    Cleanup
    Comment cleanup

--- build-vm/install.sh
+++ build-vm/install.sh
@@ -1,32 +1,23 @@
 sudo apt-get update
 
-#install Java and Maven
+#install Java, Maven, and Tomcat
 sudo apt-get install -y openjdk-7-jdk
 sudo apt-get install -y maven
+sudo apt-get install -y Tomcat
 
 # install git
 sudo apt-get install -y git
-
-# call script to install DSOL, move to vagrant file?
-#sh ./dsol.sh
 
 git clone https://github.com/SoftwareEngineeringToolDemos/ICSE-2012-DSOL.git /home/vagrant/ICSE-2012-DSOL
 chmod +x /home/vagrant/ICSE-2012-DSOL
 mv /home/vagrant/ICSE-2012-DSOL/build-vm/vm-contents/* /home/vagrant/Desktop/
 
 #set up sidebar
-gsettings reset com.canonical.Unity.Launcher favorites
-gsettings set com.canonical.Unity.Launcher favorites "['application://nautilus.desktop', 'application://firefox.desktop', 'application://gnome-terminal.desktop', 'application://unity-control-center.desktop', 'unity://running-apps', 'unity://expo-icon', 'unity://devices']"
-
-#set up sidebar
-sudo rm -f /user/share/applications/libreoffice-writer.desktop
-sudo rm -f /user/share/applications/libreoffice-calc.desktop
-sudo rm -f /user/share/applications/libreoffice-impress.desktop
-sudo rm -f /user/share/applications/amazon-default.desktop
-sudo rm -f /user/share/applications/ubuntu-software-center.desktop
-
-# Reboot the machine
-#sudo reboot
+sudo rm -f "/usr/share/applications/libreoffice-writer.desktop" 2 > /dev/null
+sudo rm -f "/usr/share/applications/libreoffice-calc.desktop" 2 > /dev/null
+sudo rm -f "/usr/share/applications/libreoffice-impress.desktop" 2 > /dev/null
+sudo rm -f "/usr/share/applications/amazon-default.desktop" 2 > /dev/null
+sudo rm -f "/usr/share/applications/ubuntu-software-center.desktop" 2 > /dev/null
 
 # build DSOL
 git clone https://github.com/leandroshp/jAMPL.git /home/vagrant/jAMPL
@@ -45,4 +36,4 @@ mvn clean compile exec:java &
 sleep 10
 
 # open DSOL web app
-#xdg-open http://localhost:8088/poll-translator/api/app
+xdg-open http://localhost:8088/poll-translator/api/app
